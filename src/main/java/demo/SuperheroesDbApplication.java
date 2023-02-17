6fe3d1423141a9757b42ea702205181b8b5be4a3
Kristian Andreasen <kristian.andreasen@fi.experis.com>
2023-02-17 13:39:23 +0100

    init

---
+++ src/main/java/demo/SuperheroesDbApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheroesDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperheroesDbApplication.class, args);
+    }
+
+}
